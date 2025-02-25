0cfd42b219940f63564fa1434ed5c60fa3e822f6
souhaibzarai <souhaibzarai18@gmail.com>
2025-02-25 02:23:34 +0000

    home implementation...

--- lib/features/home/presentation/screens/home.dart
+++ lib/features/home/presentation/screens/home.dart
@@ -1,5 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:trendbuy/common/widgets/custom_button.dart';
 import '../../../../common/app_commons.dart';
+import '../../../../utils/helpers/app_navigator.dart';
+import '../../../auth/presentation/screens/sign_in.dart';
 import '../widgets/header.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -11,7 +15,26 @@ class HomeScreen extends StatelessWidget {
       child: Scaffold(
         body: SingleChildScrollView(
           padding: AppCommons.padding,
-          child: Column(children: [Header()]),
+          child: Column(
+            children: [
+              Header(), //
+
+              SizedBox(height: 380),
+              customButton(
+                buttonText: 'Sign Out',
+                context,
+                onPressed: () async {
+                  await FirebaseAuth.instance.signOut();
+                  if (FirebaseAuth.instance.currentUser == null) {
+                    AppNavigator.pushAndRemove(
+                      context,
+                      widget: SignInScreen(), //
+                    );
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
